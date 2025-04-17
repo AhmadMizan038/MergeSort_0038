6e3c74c72805d5a2d974478b14e47c49a1991b56
AhmadMizan038 <ahmdmzn01@gmail.com>
2025-04-17 11:37:34 +0700

    membuat output ketika input elemen array

--- mergeShort.cpp
+++ mergeShort.cpp
@@ -16,3 +16,12 @@ void input() {
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+    cout << "\n==================";
+    cout << "\nInputkan Isi Element Array";
+    cout << "\n==================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Array index ke - " << i << " : ";  
+     }
+}
+
